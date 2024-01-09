d2e00ea33e960c8149480b9983662c64cf4bf8fe
다정 양 <yangdajeong3@gmail.com>
2024-01-09 15:46:21 +0900

    00. Programming

--- 00. programming/Program.cs
+++ 00. programming/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
+
+        //  1.Main 함수 필요
+        //  
+        //  static void Main(string[] args)
+        //  
+        //  프로그램은 Main 함수를 시작으로 순서대로 처리된다.
+        //  
+        
+        //  
+        //  2.표준입출력
+        //  
+        //  콘솔 : 컴퓨터와 사용자가 소통하기 위한 클래스
+        //  
+        //  Console.WriteLine  : 콘솔에 출력하고 줄을 바꿈
+        //  
+        //              Console.Write  : 콘솔에 출력하고 줄을 바꾸지 않음
+        //  
+        //              Console.ReadLine  : 콘솔을 통해 한줄 입력받음
+        //  
+        //              Console.ReadKey  : 콘솔을 통해 한키 입력받음
+        //
+
+
     }
 }
